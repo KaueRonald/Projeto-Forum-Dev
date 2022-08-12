fc591349513806d4d49b8088753a7354717455a1
Railson Mateus <raylsomatheus@hotmail.com>
2022-08-12 18:04:39 -0300

    views: Configurando o handlebars e o bootstrap

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set("port", process.env.PORT || 3000)
 // Handlebars (Template-Engine)
 app.engine("hbs", handlebars.engine({ defaultLayout: "main", extname: ".hbs" }))
 
-app.set("view", path.join(__dirname, "views"))
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "hbs")
 
 app.use(express.static(path.join(__dirname, "public")))
