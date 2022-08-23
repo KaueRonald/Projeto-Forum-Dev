f1a8985cf0d6129cd9640c205249114994f73ba5
Railson Mateus <raylsomatheus@hotmail.com>
2022-08-23 11:45:55 -0300

    env: Corrigi rotas

--- app.js
+++ app.js
@@ -4,22 +4,28 @@ const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
 const path = require("path");
 const flash = require("connect-flash");
+const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
 const mongoose = require("mongoose");
-require("./config/auth")(passport)
+require("./config/auth")(passport);
 
 const routes = require("./routes/routes");
+const routesComments = require("./routes/comments");
 
 const app = express();
-mongoose.connect("mongodb://localhost/forum-api");
+mongoose.connect("mongodb://localhost/forum-api-new");
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
 
 // Handlebars (Template-Engine)
 app.engine(
-  "hbs",
-  handlebars.engine({ defaultLayout: "main", extname: ".hbs" })
+    "hbs",
+    handlebars.engine({
+        defaultLayout: "main",
+        extname: ".hbs",
+        helpers: require("./helpers/admin.js"),
+    })
 );
 
 app.set("views", path.join(__dirname, "views"));
@@ -30,14 +36,16 @@ app.use(express.static(path.join(__dirname, "public")));
 //BodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 //Sessão
 app.use(
-  session({
-    secret: "LUp$Dg?,I#i&owP3=9su+OB%`JgL4muLF5YJ~{;t",
-    resave: false,
-    saveUninitialized: true,
-  }));
+    session({
+        secret: "LUp$Dg?,I#i&owP3=9su+OB%`JgL4muLF5YJ~{;t",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -45,17 +53,17 @@ app.use(flash());
 
 //Middleware
 app.use((req, res, next) => {
-    res.locals.success_msg = req.flash("success_msg")
-    res.locals.error_msg = req.flash("error_msg")
-    res.locals.error = req.flash("error")
-    res.locals.user = req.user || null;
-    next()
-})
-
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.use(routes);
+app.use(routesComments);
 
 //porta de acesso
 app.listen(app.get("port"), () => {
-  console.log("Server started on port " + app.get("port"));
+    console.log("Server started on port " + app.get("port"));
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,196 +1,243 @@
 const router = require("express").Router();
-const User = require("../models/User");
-const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
-const passport = require("passport")
-const {eAdmin} = require("../helpers/admin")
+const passport = require("passport");
+const { eAdmin } = require("../helpers/admin");
+const { default: mongoose } = require("mongoose");
+
+require("../models/User");
+require("../models/Post");
+require("../models/Comment");
+const User = mongoose.model("User");
+const Post = mongoose.model("Post");
+const Comment = mongoose.model("Comment");
 
 router.get("/", function (req, res) {
-
-  Post.find().lean().sort({data:"desc"}).then((users)=>{
-    res.render('home', {users: users});
-  }).catch((err)=>{
-    req.flash('error_msg', 'Houve um erro interno!')
-    res.redirect('/404')
-  })
+    Post.find()
+        .lean()
+        .sort({ data: "desc" })
+        .then((posts) => {
+            res.render("home", { posts: posts });
+        })
+        .catch((err) => {
+            req.flash("error_msg", "Houve um erro interno!");
+            res.redirect("/404");
+        });
 });
 
 router.get("/login", function (req, res) {
-  res.render("login");
+    res.render("login");
 });
 
-router.get("/profile", eAdmin, function (req, res) {
-  res.render("profile");
+router.get("/profile/:id", eAdmin, function (req, res) {
+    Comment.find({ authorId: req.params.id })
+        .lean()
+        .sort({ data: "desc" })
+        .then((comments) => {
+            Post.find({ authorId: req.params.id })
+                .lean()
+                .sort({ data: "desc" })
+                .then((posts) => {
+                    res.render("profile", { comments: comments, posts: posts });
+                });
+        });
 });
 
 router.get("/signup", function (req, res) {
-  res.render("signup");
+    res.render("signup");
 });
 
 router.post("/login", function (req, res, next) {
-
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true
-  })(req, res, next)
-
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login",
+        failureFlash: true,
+    })(req, res, next);
 });
 
-router.get("/logout", function (req, res)  {
-  req.logout(function(err) {
-    req.flash("success_msg", "Deslogado com sucesso!")
-    if (err) { return next(err); }
-    
-    res.redirect('/');
-  });
-})
+router.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        req.flash("success_msg", "Deslogado com sucesso!");
+        if (err) {
+            return next(err);
+        }
+
+        res.redirect("/");
+    });
+});
 
 router.post("/signup", function (req, res) {
-  let errs = [];
+    let errs = [];
 
-  const email = req.body.email;
-  const displayName = req.body.displayName;
-  const password = req.body.password;
-  const password2 = req.body.password2;
+    const email = req.body.email;
+    const displayName = req.body.displayName;
+    const password = req.body.password;
+    const password2 = req.body.password2;
 
-  if (!email || typeof email == undefined || email == null) {
-    errs.push({ text: "Email invalido" });
-  }
+    if (!email || typeof email == undefined || email == null) {
+        errs.push({ text: "Email invalido" });
+    }
 
-  if (!password || typeof password == undefined || password == null) {
-    errs.push({ text: "Password invalido." });
-  }
+    if (!password || typeof password == undefined || password == null) {
+        errs.push({ text: "Password invalido." });
+    }
 
-  if (password.length < 8) {
-    errs.push({ text: "Password tem que ter no mínimo 8 caracteres." });
-  }
+    if (password.length < 8) {
+        errs.push({ text: "Password tem que ter no mínimo 8 caracteres." });
+    }
 
-  if (password != password2) {
-    errs.push({ text: "Os password são diferente." });
-  }
+    if (password != password2) {
+        errs.push({ text: "Os password são diferente." });
+    }
 
-  if (errs.length > 0) {
-    res.render("signup", { errs: errs });
-  } else {
-    User.findOne({ email: email })
-      .then((user) => {
-        if (user) {
-          req.flash("error_msg", "Email já cadastrado.");
-          res.redirect("/signup");
-        } else {
-          const newUser = new User({
-            displayName: displayName,
-            email: email,
-            password: password,
-          });
+    if (errs.length > 0) {
+        res.render("signup", { errs: errs });
+    } else {
+        User.findOne({ email: email })
+            .then((user) => {
+                if (user) {
+                    req.flash("error_msg", "Email já cadastrado.");
+                    res.redirect("/signup");
+                } else {
+                    const newUser = new User({
+                        displayName: displayName,
+                        email: email,
+                        password: password,
+                    });
 
-          bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-              if (err) {
-                req.flash("error_msg", "Erro ao criar usuário.");
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) {
+                                req.flash(
+                                    "error_msg",
+                                    "Erro ao criar usuário."
+                                );
+                                res.redirect("/");
+                            }
+
+                            newUser.password = hash;
+                            newUser
+                                .save()
+                                .then((user) => {
+                                    req.flash(
+                                        "success_msg",
+                                        "Usuário criado com sucesso."
+                                    );
+                                })
+                                .catch((err) => {
+                                    req.flash(
+                                        "error_msg",
+                                        "Erro ao criar usuário, tente novamente."
+                                    );
+                                    res.redirect("/signup");
+                                });
+                        });
+                    });
+                }
+            })
+            .catch((err) => {
+                req.flash("error_msg", "Erro ao cadastrar usuário.");
                 res.redirect("/");
-              }
-
-              newUser.password = hash;
-              newUser
-                .save()
-                .then((user) => {
-                  req.flash("success_msg", "Usuário criado com sucesso.");
-                  res.redirect("/");
-                })
-                .catch((err) => {
-                  req.flash(
-                    "error_msg",
-                    "Erro ao criar usuário, tente novamente."
-                  );
-                  res.redirect("/signup");
-                });
             });
-          });
-        }
-      })
-      .catch((err) => {
-        req.flash("error_msg", "Erro ao cadastrar usuário.");
-        res.redirect("/");
-      });
-  }
+    }
+    passport.authenticate("login", {
+        successRedirect: "/",
+        failureRedirect: "/signup",
+        failureFlash: true,
+    });
 });
 
-router.get('/newPost', eAdmin, (req, res)=>{
-  res.render('newPost');
-})
+router.get("/newPost", eAdmin, (req, res) => {
+    res.render("newPost");
+});
 
-router.post('/addPost', eAdmin, (req, res)=>{
-  const newPost = {
-    title: req.body.title,
-    subTitle: req.body.subTitle,
-    description: req.body.description,
-    content: req.body.content
+router.post("/addPost", eAdmin, (req, res) => {
+    const newPost = {
+        title: req.body.title,
+        content: req.body.content,
+        authorId: req.user.id,
+        authorName: req.user.displayName,
+    };
 
-}
+    new Post(newPost)
+        .save()
+        .then(() => {
+            req.flash("success_msg", "Postagerm criada com sucesso");
+            res.redirect("/");
+        })
+        .catch((err) => {
+            req.flash("error_msg", "Houve um erro na criação da postagem");
+            res.redirect("/");
+        });
+});
 
-  new Post(newPost).save().then(() => {
-      req.flash('success_msg', 'Postagerm criada com sucesso');
-      res.redirect('/');
-  }).catch((err) => {
-      req.flash('error_msg', 'Houve um erro na criação da postagem')
-      res.redirect('/');
-  })
-})
+router.get("/postContent/:id", (req, res) => {
+    Post.findOne({ _id: req.params.id })
+        .lean()
+        .then((post) => {
+            Comment.find({ postId: req.params.id })
+                .lean()
+                .then((comments) => {
+                    res.render("postContent", {
+                        post: post,
+                        comments: comments,
+                    });
+                });
+        })
+        .catch((err) => {
+            req.flash("error_msg", "Ocorreu um erro interno");
+            res.redirect("/");
+        });
+});
 
-router.get('/postContent/:id', (req, res)=>{
-  Post.findOne({_id:req.params.id}).lean().then((post)=>{
-    res.render('postContent', {post:post});
-  }).catch((err)=>{
-    req.flash('error_msg', 'Ocorreu um erro interno');
-    res.redirect('/');
-  })
-})
+router.get("/deletePost/:id", eAdmin, (req, res) => {
+    Post.deleteMany({ _id: req.params.id })
+        .then(() => {
+            req.flash("success_msg", "Postagem deletada com sucesso!");
+            res.redirect("/");
+        })
+        .catch((err) => {
+            req.flash("error_msg", "Erro interno ao tentar deletar postagem");
+            res.redirect("/");
+        });
+});
 
-router.get('/deletePost/:id', eAdmin, (req, res)=>{
-  Post.deleteMany({_id: req.params.id}).then(()=>{
-    req.flash('success_msg', 'Postagem deletada com sucesso!')
-    res.redirect('/')
-  }).catch((err)=>{
-    req.flash('error_msg', 'Erro interno ao tentar deletar postagem')
-    res.redirect('/')
-  })
-})
+router.get("/editPost/:id", eAdmin, (req, res) => {
+    Post.findOne({ _id: req.params.id })
+        .lean()
+        .then((post) => {
+            res.render("editPost", { post: post });
+        })
+        .catch((err) => {
+            req.flash("error_msg", "Ocorreu um erro interno");
+            res.redirect("/");
+        });
+});
 
-router.get('/editPost/:id', eAdmin, (req, res)=>{
+router.post("/editPost", eAdmin, (req, res) => {
+    Post.findOne({ _id: req.body.id })
+        .then((post) => {
+            post.title = req.body.title;
+            post.subTitle = req.body.subTitle;
+            post.description = req.body.description;
+            post.content = req.body.content;
 
-  Post.findOne({_id: req.params.id}).lean().then((post) =>{
-    res.render('editPost', {post:post});
-  }).catch((err)=>{
-    req.flash('error_msg', 'Ocorreu um erro interno');
-    res.redirect('/');
-  })
-
-})
-
-router.post('/editPost', eAdmin, (req, res)=>{
-
-  Post.findOne({_id:req.body.id}).then((post)=>{
-
-    post.title = req.body.title;
-    post.subTitle = req.body.subTitle
-    post.description = req.body.description;
-    post.content = req.body.content;
-
-    post.save().then(() => {
-        req.flash('success_msg', 'Postagem editada com sucesso!')
-        res.redirect('/')
-    }).catch((err)=>{
-        req.flash('error_msg', 'Erro interno')
-        res.redirect('/');
-    })
-
-  }).catch((err)=>{
-    req.flash('error_msg', 'Ocorreu um erro ao salvar as alterações na edição da postagem')
-    res.redirect('/')
-  })
-
-})
+            post.save()
+                .then(() => {
+                    req.flash("success_msg", "Postagem editada com sucesso!");
+                    res.redirect("/");
+                })
+                .catch((err) => {
+                    req.flash("error_msg", "Erro interno");
+                    res.redirect("/");
+                });
+        })
+        .catch((err) => {
+            req.flash(
+                "error_msg",
+                "Ocorreu um erro ao salvar as alterações na edição da postagem"
+            );
+            res.redirect("/");
+        });
+});
 
 module.exports = router;
