f27f3515c25fc4d6e4a5f81a4598321d9a51b8ec
Railson Mateus <raylsomatheus@hotmail.com>
2022-08-23 11:43:41 -0300

    env: Modifica model post

--- models/Post.js
+++ models/Post.js
@@ -1,28 +1,32 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const postSchema = new Schema({
-  title: {
-    type: String
-  },
-  subTitle: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  }
+    title: {
+        type: String,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    authorName: {
+        type: String,
+        required: true,
+    },
+    authorId: {
+        type: String,
+        required: true,
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+        },
+    ],
+    date: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 const Post = mongoose.model("Post", postSchema);
